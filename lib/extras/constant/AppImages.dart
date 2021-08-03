9ed3d6506886ac1f4d1cb48fb6d60d8bb397578e
2ndofIcarus <77532462+2ndofIcarus@users.noreply.github.com>
2021-08-03 21:04:08 +0800

    Revert "Merge branch 'Dev_Ralph' into main"
    This reverts commit c477fc9a73a4cf268db034d713fe3a6b77bc7d62, reversing
    changes made to b42d204ab884b520552a2030aaba0fec6370c8ce.

--- lib/extras/constant/AppImages.dart
+++ lib/extras/constant/AppImages.dart
@@ -1,8 +1,2 @@
 //Sample Code
 //const walkThrough1 = "assets/walkthrough1.svg";
-
-const calendarIcon = "assets/icons/calendar.svg";
-const clockIcon = "assets/icons/clock.svg";
-const moneyIcon = "assets/icons/money.svg";
-
-const backgroundImage = "assets/background.png";
