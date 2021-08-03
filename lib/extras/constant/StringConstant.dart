981e88adbec03f0a852c270a11232a245f58b92a
2ndofIcarus <77532462+2ndofIcarus@users.noreply.github.com>
2021-08-03 22:15:40 +0800

    fixes

--- lib/extras/constant/StringConstant.dart
+++ lib/extras/constant/StringConstant.dart
@@ -4,8 +4,6 @@
 // use lbl for Label, txt for TextField, btn for Button
 // sample lblName, txtComment, btnLogin
 
-//Therapist Screen
-const Test = "Test";
 const appbarTitle = "Zen Body Massage";
 const btnBookMe = "Book Me";
 const nameTest = "Anna White";
